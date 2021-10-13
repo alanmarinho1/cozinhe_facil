fc692c608779c1ae6a0f8720f2a33d0ca5524d74
Alan Marinho <kgometalenator@gmail.com>
2021-10-13 07:24:50 -0300

    Navegação telas

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import {AppRegistry} from 'react-native';
 import Login from './screens/Login';
 import Entrar from './screens/Entrar';
 import Registrar from './screens/Registrar';
+import App from './screens/TestLogin';
 import {name as appName} from './app.json';
+import { NavigationContainer } from '@react-navigation/native';
 
-AppRegistry.registerComponent(appName, () => Entrar);
+<NavigationContainer>{AppRegistry.registerComponent(appName, () => App)}</NavigationContainer>
+//AppRegistry.registerComponent(appName, () => App);
--- screens/TestLogin.js
+++ screens/TestLogin.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import auth from '@react-native-firebase/auth';
 
-export function App() {
+export default function App() {
   // Set an initializing state whilst Firebase connects
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
