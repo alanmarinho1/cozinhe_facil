c116cf90e98ed8da9cae3d951336ad15b9f1b045
Alan Marinho <kgometalenator@gmail.com>
2021-11-11 23:08:31 -0300

    Checkbox

--- screens/Preparo/index.js
+++ screens/Preparo/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text,
   View,
   StatusBar,
-  FlatList,
+  FlatList, 
 } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
 import {ImageTopo} from './styles'
 
+
 export default function Preparo(){
+    const [toggleCheckBox, setToggleCheckBox] = useState(false)
+
     return(
         <View style={{backgroundColor: '#FFF5EB'}}>
             <StatusBar backgroundColor= 'transparent'/>
             <ImageTopo source={require('../../assets/macarronada.jpg')}/>
+            <CheckBox
+                style={{top: 200}}
+                disabled={false}
+                value={toggleCheckBox}
+                onValueChange={(newValue) => setToggleCheckBox(newValue)}
+                
+
+            />
+            
             
 
         </View>
